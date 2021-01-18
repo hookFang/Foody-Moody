f95baef3a3e157652360f78da70a588b2fed6e88
Meisam <200412272@student.georgianc.on.ca>
2021-01-18 13:40:44 -0500

    First update
    Added the first page includes the landscape view.

--- app/src/main/java/com/bluegeeks/foodymoody/MainActivity.kt
+++ app/src/main/java/com/bluegeeks/foodymoody/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.bluegeeks.foodymoody
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val timer = Timer()
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+
+                val i = Intent(applicationContext, LoginActivity::class.java)
+
+                startActivity(i)
+            }
+        }, 2000)
     }
 }
