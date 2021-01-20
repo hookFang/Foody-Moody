5625e4ed841315d659209a895d4dd68c5d939201
Edwin Joseph Christie <48800431+hookFang@users.noreply.github.com>
2021-01-20 14:05:28 -0500

    Login page (#2)
    I created login page, it may need to shave later
    
    Co-authored-by: Meisam <200412272@student.georgianc.on.ca>

--- app/src/main/java/com/bluegeeks/foodymoody/LoginActivity.kt
+++ app/src/main/java/com/bluegeeks/foodymoody/LoginActivity.kt
@@ -1,11 +1,34 @@
 package com.bluegeeks.foodymoody
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        var clicked = false
+
+        button_visiblity.setOnClickListener {
+            if(editText_password.text.toString().isNotEmpty()) {
+                if (!clicked) {
+                    editText_password.transformationMethod =
+                        HideReturnsTransformationMethod.getInstance()
+                    button_visiblity.setBackgroundResource(R.drawable.invisible)
+                    clicked = true
+                } else {
+                    editText_password.transformationMethod =
+                        PasswordTransformationMethod.getInstance()
+                    button_visiblity.setBackgroundResource(R.drawable.visible)
+                    clicked = false
+                }
+            }
+        }
     }
 }
