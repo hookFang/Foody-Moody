002321d5d3cbe80ebadf123f14e8cd815bdd997d
Edwin Christie <edwinchristie100@gmail.com>
2021-02-06 10:21:15 -0500

    Added features to select images from camera and storage.

--- app/src/main/java/com/bluegeeks/foodymoody/PostActivity.kt
+++ app/src/main/java/com/bluegeeks/foodymoody/PostActivity.kt
@@ -2,25 +2,34 @@ package com.bluegeeks.foodymoody
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
 import androidx.annotation.RequiresApi
-import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
 import kotlinx.android.synthetic.main.activity_post.*
 import kotlinx.android.synthetic.main.toolbar_main.*
+import java.io.ByteArrayOutputStream
 import java.text.SimpleDateFormat
 import java.util.*
 
-class PostActivity : AppCompatActivity() {
 
-    // FirebaseAuth instance
-    private val authDb = FirebaseAuth.getInstance()
-    val db = FirebaseFirestore.getInstance().collection("posts")
+class PostActivity : BaseFirebaseProperties() {
+    val db = rootDB.collection("posts")
+    //bitmap image fore the post
+    var bitmapImage: Bitmap? = null
+    var storage = FirebaseStorage.getInstance()
+    var storageRef = storage.reference
 
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,36 +37,63 @@ class PostActivity : AppCompatActivity() {
         setContentView(R.layout.activity_post)
 
         val successMessage = "Post submitted successfully"
-        val unSuccessMessage = "Posting was unsuccessful"
+        val unSuccessMessage = "Error, Cannot Post"
 
+        //Pick image from gallery or photo
         imageView_post.setOnClickListener {
+            val options = arrayOf<CharSequence>("Take Photo", "Choose from Gallery", "Cancel")
 
+            val builder: android.app.AlertDialog.Builder = android.app.AlertDialog.Builder(this)
+            builder.setTitle("Select an Option")
+            builder.setItems(options) { dialog, item ->
+                when (options[item]) {
+                    "Take Photo" -> {
+                        val takePicture = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                        startActivityForResult(takePicture, 1002)
+                    }
+                    "Choose from Gallery" -> {
+                        val pickPhoto = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                        startActivityForResult(pickPhoto, 1001)
+                    }
+                    "Cancel" -> {
+                        dialog.dismiss()
+                    }
+                }
+            }
+            builder.show()
         }
 
         button_post.setOnClickListener {
-            if (authDb.currentUser != null) {
-
-                try {
+            progressBar_PostPage.visibility = View.VISIBLE;
+            try {
                     val post = Post()
                     post.userId = authDb.currentUser!!.uid
                     post.description = editText_description.text.toString().trim()
                     post.time = getTime()
-    
                     post.id = db.document().id
                     db.document(post.id!!).set(post)
-    
-                    // show confirmation & clear inputs
-                    editText_description.setText("")
-    
-                    Toast.makeText(this, successMessage, Toast.LENGTH_LONG).show()
-                } catch (e: Exception) {
-                    Toast.makeText(this, unSuccessMessage + " :" + e, Toast.LENGTH_LONG).show()
-                }
-            } else {
-                logout()
-            }
-        }
 
+                    //Upload the image to firebase storage
+                    val imagesRef : StorageReference = storageRef.child("images/" + post.id + ".jpeg")
+                    val baos = ByteArrayOutputStream()
+                    bitmapImage?.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+                    val data = baos.toByteArray()
+                    val uploadTask: UploadTask = imagesRef.putBytes(data)
+                    uploadTask.addOnFailureListener {
+                        Toast.makeText(this, unSuccessMessage, Toast.LENGTH_LONG).show()
+                        progressBar_PostPage.visibility = View.GONE
+                    }.addOnSuccessListener {
+                        Toast.makeText(this, successMessage, Toast.LENGTH_LONG).show()
+                        progressBar_PostPage.visibility = View.GONE
+                        finish()
+                    }
+                } catch (e: Exception) {
+                    Log.e("TAG", e.message!!)
+                    Toast.makeText(this, unSuccessMessage , Toast.LENGTH_LONG).show()
+                    progressBar_PostPage.visibility = View.GONE
+                    finish()
+                }
+        }
         //instantiate toolbar
         setSupportActionBar(topToolbar)
     }
@@ -68,14 +104,12 @@ class PostActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
         when (item.itemId) {
             R.id.action_back -> {
                 startActivity(Intent(applicationContext, HomeActivity::class.java))
                 return true
             }
         }
-
         return super.onOptionsItemSelected(item)
     }
 
@@ -86,11 +120,25 @@ class PostActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
+    //To get the image from the gallery
+    @RequiresApi(Build.VERSION_CODES.P)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1001 && data != null) {
+            val selectedImage: Uri? = data.data
+            bitmapImage = selectedImage?.let { ImageDecoder.createSource(this.contentResolver, it) }?.let { ImageDecoder.decodeBitmap(it) }
+            imageView_post.setImageBitmap(bitmapImage)
+        } else if (requestCode == 1002 && data != null) {
+            bitmapImage = data.extras?.get("data") as Bitmap
+            print(bitmapImage)
+            imageView_post.setImageBitmap(bitmapImage)
+        }
+    }
+
     @SuppressLint("SimpleDateFormat")
     fun getTime(): String {
         val sdf = SimpleDateFormat("yyyy.MM.dd G 'at' HH:mm:ss z")
         val currentDateandTime: String = sdf.format(Date())
-
         return currentDateandTime
     }
 }
